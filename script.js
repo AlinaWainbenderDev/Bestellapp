df9a74861c17f2597ef7f9f5bb8de18f379a7e73
Alina Wainbender <a.wainbender@web.de>
2025-09-08 18:50:11 +0200

    cart updated: added remove and delete functionality; adjusted styles

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
-function renderDishes(){
-    let mainContainerRef = document.getElementById("menu");
-    mainContainerRef.innerHTML = "";
 
-    for (let index = 0; index < dishes.length; index++) {
-        const dishTemplate = templateDishes(dishes[index]);
-
-        mainContainerRef.innerHTML += dishTemplate;
-    }
+function init(){
+    renderMenu();
 }
 
-renderDishes();
+function renderMenu(){
+    let menuRef = document.getElementById('menu');
+
+    let titleMenu = `<h2>Hauptgerichte</h2>`;
+
+    for (let index = 0; index < dishes.length; index++) {
+        titleMenu += templateDishes(dishes[index], index);
+    }
+
+    menuRef.innerHTML = titleMenu;
+}
 
 function toggleOverlay(){
     let overlayRef = document.getElementById("overlay");
     overlayRef.classList.toggle("open")
 }
+
+function addDishToBasket(index){
+    let basketDishRef = document.getElementById(`basketDish-${index}`);
+
+    if (basketDishRef == null) {
+        let amountDishesBasket = 1;
+        templateBasket(dishes[index], amountDishesBasket, index);
+    } else {
+        let amountRef = document.getElementById(`amount-${index}`);
+        let currentAmount = parseInt(amountRef.innerText);
+
+        if (currentAmount < 10) {
+            amountRef.innerText = currentAmount + 1;
+            updateSum(index);
+        }
+    }
+}    
+
+function reduceAmountBasket(index){
+    let amountRef = document.getElementById(`amount-${index}`);
+    let currentAmount = parseInt(amountRef.innerText);
+
+    if (currentAmount <= 1) {
+        removeDishFromBasket(index);
+    } else {
+        amountRef.innerText = currentAmount - 1;
+        updateSum(index);
+    }
+}
+
+function removeDishFromBasket(index){
+    let basketDishRef = document.getElementById(`basketDish-${index}`);
+    basketDishRef.remove();
+}
+
+function updateSum(index) {
+    const amountRef = document.getElementById(`amount-${index}`);
+    const sumRef = document.getElementById(`sum-${index}`);
+    const currentAmount = parseInt(amountRef.innerText);
+    const price = dishes[index].price;
+
+    sumRef.innerText = (currentAmount * price).toFixed(2) + " €";
+}
+
+//erstmal count per dish 
+// danach total price
+// danach checkout button 
+// danach funktion für checkout button ( danke für die bestellung, warenkorb leeren, overlay schließen)
--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,36 @@
-function templateDishes(dishes){
+let index=0;
+
+function templateDishes(dish, index){
     return `
      <div class="dish">
-        <h2>${dishes.name}</h2>
-        <p>${dishes.description}</p>
+        <div class="style-dishContainer">
+            <h3>${dish.name}</h3>
+            <button onclick="addDishToBasket(${index})" id="addButton-${index}" class="add-button">
+                <span class="horizontal"></span>
+                <span class="vertical"></span>
+            </button>
+        </div>
+        <p>${dish.description}</p>
+        <p class="dish-price">${dish.price} €</p>
      </div>
     `
+}
+
+function templateBasket(dish, amountDishesBasket, index){
+    let contentOverlayRef = document.getElementById("contentOverlay");
+    contentOverlayRef.innerHTML += `
+       <div class="basketDishContainer" id="basketDish-${index}">${dish.name}
+            <div class="basket-dish-area">
+                <button onclick="reduceAmountBasket(${index})">-</button>
+                <span id="amount-${index}">${amountDishesBasket}</span>
+                <button onclick="addDishToBasket(${index})">+</button>
+                <span id="sum-${index}"></span>
+                <button onclick="removeDishFromBasket(${index})">
+                    <img class="trash-icon" src="/assets/icons/recycle-bin-icon.svg" alt="Trash Icon">
+                </button>
+            </div>
+        </div>
+    `;
+
+    updateSum(index);
 }
